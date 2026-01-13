1765f072c9f6373e751f0b4b75bd6c6bf6d2dcba
moveirama <moveirama@gmail.com>
2026-01-13 17:10:28 -0300

    pagina-produtos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'artely.com.br',
+        pathname: '/site/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
