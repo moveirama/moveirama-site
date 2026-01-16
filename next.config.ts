f0e02b06d39dd5a9289ef651ef1175e927ba4270
moveirama <moveirama@gmail.com>
2026-01-16 11:18:52 -0300

    chore: remove unused image domains

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'artely.com.br',
-        pathname: '/site/wp-content/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'ewsmfvisypgxbeqtbmec.supabase.co',
         pathname: '/storage/**',
       },
