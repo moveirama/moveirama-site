d3fdfa46ad45864710e27669bc17b8bce7573244
moveirama <moveirama@gmail.com>
2026-01-22 21:17:17 -0300

    Update index.ts

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -11,13 +11,13 @@ export { default as TrustBar } from './TrustBar';
 export { default as CategoriesSection } from './CategoriesSection';
 export { default as WhatsAppFloat } from './WhatsAppFloat';
 
-// Bloco 2 - Implementado
-export { default as FeaturedProducts } from './FeaturedProducts';
-export { default as DiferenciaisSection } from './DiferenciaisSection';
-export { default as KnowledgeBlock } from './KnowledgeBlock';
+// Bloco 2 - Pendente
+// export { default as FeaturedProducts } from './FeaturedProducts';
+// export { default as DiferenciaisSection } from './DiferenciaisSection';
+// export { default as KnowledgeBlock } from './KnowledgeBlock';
 
-// Bloco 3 - Implementado
-export { default as HomeFAQ } from './HomeFAQ';
-export { default as CoberturaSection } from './CoberturaSection';
-export { default as SocialSection } from './SocialSection';
-export { default as CTAFinal } from './CTAFinal';
+// Bloco 3 - Pendente
+// export { default as HomeFAQ } from './HomeFAQ';
+// export { default as CoberturaSection } from './CoberturaSection';
+// export { default as SocialSection } from './SocialSection';
+// export { default as CTAFinal } from './CTAFinal';
