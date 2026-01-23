0d57dfea78c80d68323c53e62af9f5bb3592e23b
moveirama <moveirama@gmail.com>
2026-01-23 01:11:27 -0300

    Update index.ts

--- src/components/ofertas/index.ts
+++ src/components/ofertas/index.ts
@@ -1,1 +1,10 @@
+/**
+ * Barrel export - Componentes de Ofertas
+ * Squad Dev - Janeiro 2026
+ */
 
+export { default as OfertasHero } from './OfertasHero';
+export { default as TabelaSemantica } from './TabelaSemantica';
+export { default as OfertasFiltros } from './OfertasFiltros';
+export { default as OfertaProductCard } from './OfertaProductCard';
+export { default as OfertasConfianca } from './OfertasConfianca';
