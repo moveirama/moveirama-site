e90ec8460a2954c5e4d7b035b224dd1e878edc88
Sandro <usecatchlead@gmail.com>
2026-01-30 18:03:33 -0300

    fix: remove duplicate exports in checkout index

--- src/components/checkout/index.ts
+++ src/components/checkout/index.ts
@@ -1,10 +1,9 @@
 // src/components/checkout/index.ts
 // Barrel export for checkout components
 
-export { default as CheckoutLayout, CheckoutLayout } from './CheckoutLayout'
-export { default as CheckoutTrustBar, CheckoutTrustBar } from './CheckoutTrustBar'
-export { default as CheckoutSteps, CheckoutSteps } from './CheckoutSteps'
-export { default as CheckoutSummaryCard, CheckoutSummaryCard } from './CheckoutSummaryCard'
-export { default as CheckoutMiniSummary, CheckoutMiniSummary } from './CheckoutMiniSummary'
-export { default as CheckoutSeals, CheckoutSeals } from './CheckoutSeals'
-export { default as CheckoutIdentity, CheckoutIdentity } from './CheckoutIdentity'
+export { default as CheckoutLayout } from './CheckoutLayout'
+export { default as CheckoutTrustBar } from './CheckoutTrustBar'
+export { default as CheckoutSteps } from './CheckoutSteps'
+export { default as CheckoutSummaryCard } from './CheckoutSummaryCard'
+export { default as CheckoutSeals } from './CheckoutSeals'
+export { default as CheckoutIdentity } from './CheckoutIdentity'
